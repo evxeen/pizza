83caf4ac3c0c8a75cee4d273dfc4b470cd00750d
evxeen <evxeen@gmail.com>
2024-06-28 13:38:52 +0300

    feat: secelt category

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,21 @@
+import { useState } from 'react';
+
 function Categories() {
+  const [activeCategory, setActiveCategory] = useState(0);
+  const categoties = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+
+  const selectCategory = (id) => {
+    setActiveCategory(id);
+  };
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categoties.map((el, id) => (
+          <li onClick={() => selectCategory(id)} className={activeCategory === id ? 'active' : ''}>
+            {el}
+          </li>
+        ))}
       </ul>
     </div>
   );
