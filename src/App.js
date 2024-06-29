45d489942b4e78c09fba8b8c0d84015940748593
evxeen <evxeen@gmail.com>
2024-06-29 16:14:32 +0300

    add: pages & routes

--- src/App.js
+++ src/App.js
@@ -1,42 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import Categories from './components/Categories';
-import Sorting from './components/Sorting';
-import PizzaCard from './components/PizzaCard';
+import Main from './pages/Main';
+import Cart from './pages/Cart';
 
 import './scss/app.scss';
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch('https://667f0707f2cb59c38dc7d61e.mockapi.io/items')
-      .then((res) => res.json())
-      .then((data) => setData(data))
-      .catch((error) => console.error('Error:', error));
-  }, []);
-
-  fetch('https://667f0707f2cb59c38dc7d61e.mockapi.io/items')
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.error('Error:', error));
-
   return (
     <div className="App">
       <div className="wrapper">
         <Header />
         <div className="content">
           <div className="container">
-            <div className="content__top">
-              <Categories />
-              <Sorting />
-            </div>
-            <h2 className="content__title">Все пиццы</h2>
-            <div className="content__items">
-              {data.map((obj) => (
-                <PizzaCard key={obj.id} {...obj} />
-              ))}
-            </div>
+            <Routes>
+              <Route path="/" element={<Main />} />
+              <Route path="/cart" element={<Cart />} />
+            </Routes>
           </div>
         </div>
       </div>
---
+++ src/pages/Main.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+import Categories from '../components/Categories';
+import Sorting from '../components/Sorting';
+import PizzaCard from '../components/PizzaCard';
+
+function Main() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://667f0707f2cb59c38dc7d61e.mockapi.io/items')
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error('Error:', error));
+  }, []);
+  return (
+    <>
+      <div className="content__top">
+        <Categories />
+        <Sorting />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {data.map((obj) => (
+          <PizzaCard key={obj.id} {...obj} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default Main;
