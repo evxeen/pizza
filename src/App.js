bd3d4bd55ab3d6fb93ed1db072b172d16aeba933
evxeen <evxeen@gmail.com>
2024-06-29 12:59:17 +0300

    feat: geting data from server

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Sorting from './components/Sorting';
 import PizzaCard from './components/PizzaCard';
 
-import data from './assets/db-1637762081903.json';
 import './scss/app.scss';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://667f0707f2cb59c38dc7d61e.mockapi.io/items')
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error('Error:', error));
+  }, []);
+
+  fetch('https://667f0707f2cb59c38dc7d61e.mockapi.io/items')
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error('Error:', error));
+
   return (
     <div className="App">
       <div className="wrapper">
